10fe5c6abbc532bb984ab04aa0eb665875baecc2
NielsVandenBroeck <nielsvandenbroeck11@gmail.com>
2025-03-01 11:19:53 +0100

    endpoint getplants

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,6 +2,8 @@ from typing import Union
 
 from fastapi import FastAPI, HTTPException
 from DataSetGenerators.Weather import WeatherDatasetGenerator
+from DataSetGenerators.Planter import *
+
 
 app = FastAPI()
 
@@ -12,9 +14,9 @@ def read_root():
 
 @app.get("/getWeather/{location}/{day}")
 def get_weather(location: str, day: int, q: Union[str, None] = None):
+    #todo change to make all days
     if day < 1 or day > 365:
         raise HTTPException(status_code=400, detail="Day must be between 1 and 365")
-
     if location not in weather.locations:
         raise HTTPException(status_code=400, detail=f"Invalid location: {location}")
 
@@ -23,3 +25,7 @@ def get_weather(location: str, day: int, q: Union[str, None] = None):
         return weather_data
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+@app.get("/getPlants/{location}}")
+def get_plants(location: str, q: Union[str, None] = None):
+    return 0
